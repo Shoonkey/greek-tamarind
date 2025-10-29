061d34cc08f3b7acae24dfd1eb49aca20dee6c0b
Shoonkey <shoonkey.dev@gmail.com>
2025-10-29 20:16:49 -0300

    chore(pagination-control): removed arbitrary page option

--- src/app/components/pagination-control/pagination-control.ts
+++ src/app/components/pagination-control/pagination-control.ts
@@ -1,36 +1,16 @@
-import { Component, input, output, signal } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { MatInput } from '@angular/material/input';
-import { MatFormField } from '@angular/material/form-field';
 import { MatTooltip } from '@angular/material/tooltip';
 
 @Component({
   selector: 'app-pagination-control',
-  imports: [MatIconButton, MatIcon, MatInput, MatFormField, MatTooltip],
+  imports: [MatIconButton, MatIcon, MatTooltip],
   templateUrl: './pagination-control.html',
   styleUrl: './pagination-control.scss',
 })
 export class PaginationControl {
   currentPage = input.required<number>();
   totalPages = input.required<number>();
-
-  editing = signal<boolean>(false);
-
   pageChanged = output<number>();
-
-  toggleInputMode() {
-    this.editing.update((isEditing) => !isEditing);
-  }
-
-  handlePageFormSubmit(e: SubmitEvent) {
-    e.preventDefault();
-
-    const form = e.target as HTMLFormElement;
-    const pageInput = form.elements[0] as HTMLInputElement;
-
-    const newPage = parseInt(pageInput.value);
-    this.pageChanged.emit(newPage);
-    this.editing.set(false);
-  }
 }
