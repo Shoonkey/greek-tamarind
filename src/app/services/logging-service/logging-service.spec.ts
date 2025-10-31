27ad902971f1b1ea13192ba90f03b8226a596090
Shoonkey <shoonkey.dev@gmail.com>
2025-10-31 18:24:10 -0300

    test: add unit tests for LoggingService

--- src/app/services/logging-service/logging-service.spec.ts
+++ src/app/services/logging-service/logging-service.spec.ts
@@ -1,32 +1,91 @@
 import { TestBed } from '@angular/core/testing';
+import { provideZonelessChangeDetection } from '@angular/core';
 
 import { LoggingService } from './logging-service';
-import { provideZonelessChangeDetection } from '@angular/core';
+import { EnvironmentService } from '../environment-service/environment-service';
+
+function provideMockEnvironment({ prod }: { prod: boolean }) {
+  return { isProduction: () => prod };
+}
 
 describe('LoggingService', () => {
   let service: LoggingService;
+  let consoleSpy: jasmine.SpyObj<Console>;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [provideZonelessChangeDetection()],
+  describe('in development mode', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          provideZonelessChangeDetection(),
+          { provide: EnvironmentService, useValue: provideMockEnvironment({ prod: false }) },
+        ],
+      });
+
+      service = TestBed.inject(LoggingService);
+      consoleSpy = spyOnAllFunctions(console);
     });
-    service = TestBed.inject(LoggingService);
+
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+
+    describe('logInfo', () => {
+      it('should call console.log with passed arguments and a dev-only log notice in development mode', () => {
+        const args = ['testing', 'it'];
+        service.logInfo(...args);
+        expect(consoleSpy.log).toHaveBeenCalledOnceWith('[dev-only log]', ...args);
+      });
+
+      it('should not call console.error', () => {
+        service.logInfo('test');
+        expect(consoleSpy.error).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('logError', () => {
+      it('should call console.error with passed arguments and a dev-only log notice in development mode', () => {
+        const args = ['testing', 'it'];
+
+        service.logError(...args);
+        expect(consoleSpy.error).toHaveBeenCalledOnceWith('[dev-only log]', ...args);
+      });
+
+      it('should not call console.log', () => {
+        service.logError('test');
+        expect(consoleSpy.log).not.toHaveBeenCalled();
+      });
+    });
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  describe('in production mode', () => {
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          provideZonelessChangeDetection(),
+          { provide: EnvironmentService, useValue: provideMockEnvironment({ prod: true }) },
+        ],
+      });
+
+      service = TestBed.inject(LoggingService);
+      consoleSpy = spyOnAllFunctions(console);
+    });
+
+    it('should be created', () => {
+      expect(service).toBeTruthy();
+    });
+
+    describe('logInfo', () => {
+      it('should not call console.error in production', () => {
+        service.logError('test');
+        expect(consoleSpy.error).not.toHaveBeenCalled();
+      });
+    });
+
+    describe('logError', () => {
+      it('should not call console.error in production', () => {
+        service.logError('test');
+        expect(consoleSpy.error).not.toHaveBeenCalled();
+      });
+    });
   });
-
-  // TODO: implement tests
-  // describe('logInfo', () => {
-  //   it('should not call console.error', () => {});
-  //   it('should call console.log with passed arguments in development mode', () => {});
-  //   it('should not call console.log in production', () => {});
-  // });
-
-  // describe('logError', () => {
-  //   it('should not call console.log');
-  //   it('should call console.error with passed arguments in development mode', () => {});
-  //   it('should not call console.error in production', () => {});
-  // });
 });
