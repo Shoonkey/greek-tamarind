de6e511ec0ae9dca6e5c32c4ecf8625ce04d81dd
Shoonkey <shoonkey.dev@gmail.com>
2025-11-14 20:24:56 -0300

    chore(interceptor/log): remove redundancy
    The logging service already only logs when out of a production environment

--- src/app/interceptors/logging.interceptor.ts
+++ src/app/interceptors/logging.interceptor.ts
@@ -1,16 +1,11 @@
 import { inject } from '@angular/core';
 
-import { AngularHttpInterceptor } from '../models/AngularHttpInterceptor';
 import { LoggingService } from '../services/logging-service/logging-service';
-import { EnvironmentService } from '../services/environment-service/environment-service';
+import { AngularHttpInterceptor } from '../models/AngularHttpInterceptor';
 
 export const loggingInterceptor: AngularHttpInterceptor = (req, next) => {
-  const environment = inject(EnvironmentService);
-
-  if (!environment.isProduction()) {
-    const loggingService = inject(LoggingService);
-    loggingService.logInfo(`API request at: ${req.url}`);
-  }
+  const loggingService = inject(LoggingService);
+  loggingService.logInfo(`API request at: ${req.url}`);
 
   return next(req);
 };
