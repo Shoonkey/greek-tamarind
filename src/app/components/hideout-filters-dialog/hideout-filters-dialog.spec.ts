4936882ba0d2e59b3d1922fc876e4c7250386826
Shoonkey <shoonkey.dev@gmail.com>
2025-11-14 20:52:57 -0300

    chore: lay out needed tests for HideoutFiltersDialog

--- src/app/components/hideout-filters-dialog/hideout-filters-dialog.spec.ts
+++ src/app/components/hideout-filters-dialog/hideout-filters-dialog.spec.ts
@@ -37,4 +37,58 @@ describe('HideoutFiltersDialog', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // it("should have the title 'Filters'", () => {});
+
+  // it('should receive and save `baseData`', () => {});
+  // it('should receive and save `filters` in a signal', () => {});
+
+  // it('should close on save, emitting the currently selected filters', () => {});
+  // it('should close on cancel, emitting nothing (undefined)', () => {});
+
+  // describe('name input', () => {
+  //   it('should exist', () => {});
+  //   it('should have proper label', () => {});
+  //   it('should be bound to `filters.name`', () => {});
+  //   it('filters.name should be null when the filters.name = null', () => {});
+  // });
+
+  // describe('uses MTX button group', () => {
+  //   it('should exist', () => {});
+  //   it('should have proper label', () => {});
+  //   it("shouldn't allow for the deselection of both options", () => {});
+
+  //   describe('should have both true and false selected', () => {
+  //     it('at first', () => {});
+  //     it('when filters.hasMTX is falsy', () => {});
+  //   });
+  // });
+
+  // describe('PoE version button group', () => {
+  //   it('should exist', () => {});
+  //   it('should have proper label', () => {});
+
+  //   it("shouldn't allow for the deselection of both options", () => {});
+
+  //   describe('should have both versions selected', () => {
+  //     it('at first', () => {});
+  //     it('when filters.poeVersion is falsy', () => {});
+  //   });
+  // });
+
+  // describe('maps ACMS chip filter', () => {
+  //   it('should exist', () => {});
+  //   it('should have proper label', () => {});
+  //   it('should pass `items` (list of all items) correctly', () => {});
+  //   it('should be bound to filters.mapIds', () => {});
+  //   it('filters.mapIds should be null in case no map is selected');
+  // });
+
+  // describe('Tags ACMS chip filter', () => {
+  //   it('should exist', () => {});
+  //   it('should have proper label', () => {});
+  //   it('should pass `items` (list of all items) correctly', () => {});
+  //   it('should be bound to filters.tagIds', () => {});
+  //   it('filters.tagIds should be null in case no tag is selected', () => {});
+  // });
 });
